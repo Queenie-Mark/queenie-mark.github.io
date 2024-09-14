e21c1975acb826f67a8cd0fb2cfaa0d8c6c83034
Queenie-Mark <qmark6609@gmail.com>
2024-09-14 21:14:53 +0100

    Trial

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
 const images = document.querySelectorAll ("img") ;
 
 for (const image of images){
-    fetch("https://dog.ceo/api/breeds/image/random").then(response => response.json())
+    fetch("https://unsplash.com/images/nature/flower").then(response => response.json())
     .then(data =>
         {image.src =data.message 
             image.width = 100 ;
